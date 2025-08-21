c5e18476af0d2fce1779d11a1484c0875cdfaec3
iAmSomeBoy <ashikur2401@gmail.com>
2025-08-21 17:28:58 +0600

    space omitted

--- budget.h
+++ budget.h
@@ -10,7 +10,7 @@ public:
     void addIncome(double v) { income += v; }
     void addExpense(std::string c, double v) { spent += v; cat[c]+=v; }
     void report() {
-        std::cout << "Income: " << income << "\nSpent: " << spent << "\nRemaining: " << income - spent << "\n";
-        for(auto &p:cat) std::cout << p.first << ": " << p.second << "\n";
+        std::cout<<"Income:"<<income<<"\nSpent:"<<spent<<"\nRemaining: " << income - spent << "\n";
+        for(auto &p:cat)std::cout<<p.first <<":"<<p.second <<"\n";
     }
 };
