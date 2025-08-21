64de820a36b3cb3597ffb5786c86e1f1375b914e
iAmSomeBoy <ashikur2401@gmail.com>
2025-08-21 17:31:12 +0600

    ed

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 #include<bits/stdc++.h>
 #include "user.h"
 int main() {
-    std::string uid,pass;
-    std::cout<<"===Welcome to EDU-SYNC===\n";
-    std::cout<<"Enter Id: ";
-    std::cin>>uid;
-    std::cout<<"Enter password: ";
-    std::cin>>pass;
-    std::cin.ignore();
-    std::cout<<"Who are you? (1->Student,2->Teacher,3->Friend)\n";
+    string uid,pass;
+    cout<<"===Welcome to EDU-SYNC===\n";
+    cout<<"Enter Id: ";
+    cin>>uid;
+    cout<<"Enter password: ";
+    cin>>pass;
+    cin.ignore();
+    cout<<"Who are you? (1->Student,2->Teacher,3->Friend)\n";
     int c;
-    std::cin>>c;
+    cin>>c;
 
     User *u=NULL;
     if(c==1) {
